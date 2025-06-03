bd22065cc55f4b180bb8c648d5cb59ad03b31a13
Jonathan Sauter <jonathansauter@Jonathans-MBP-2.local>
2025-06-02 21:32:43 -0600

    add bird logic

--- components.py
+++ components.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 
 class Ground:
@@ -10,3 +11,33 @@ class Ground:
 
     def draw(self, window):
         pygame.draw.rect(window, "white", self.rect)
+
+
+class Pipes:
+    width = 15
+    opening = 100
+
+    def __init__(self, win_width):
+        self.x = win_width
+        self.bottom_height = random.randint(10, 300)
+        self.top_height = Ground.ground_level - self.bottom_height - self.opening
+        self.bottom_rect, self.top_rect = pygame.Rect(
+            0, 0, 0, 0), pygame.Rect(0, 0, 0, 0)
+        self.passed = False
+        self.off_screen = False
+
+    def draw(self, window):
+        self.bottom_rect = pygame.Rect(
+            self.x, Ground.ground_level - self.bottom_height, self.width, self.bottom_height)
+        pygame.draw.rect(window, "white", self.bottom_rect)
+
+        self.top_rect = pygame.Rect(
+            self.x, 0, self.width, self.top_height)
+        pygame.draw.rect(window, "white", self.top_rect)
+
+    def update(self):
+        self.x -= 1
+        if self.x + Pipes.width <= 50:
+            self.passed = True
+        if self.x <= -Pipes.width:
+            self.off_screen = True
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pygame
 from sys import exit
 import config
+import components
+import population
 
 pygame.init()
 clock = pygame.time.Clock()
+population = population.Population(100)
 
 
 def quit_game():
@@ -13,13 +16,35 @@ def quit_game():
             exit()
 
 
+def generate_pipes():
+    config.pipes.append(components.Pipes(config.win_width))
+
+
 def main():
+    pipes_spawn_time = 10
     while True:
         quit_game()
 
         config.window.fill("black")
 
         config.ground.draw(config.window)
+
+        if pipes_spawn_time <= 0:
+            generate_pipes()
+            pipes_spawn_time = 200
+
+        pipes_spawn_time -= 1
+
+        for p in config.pipes:
+            p.draw(config.window)
+            p.update()
+            if p.off_screen:
+                config.pipes.remove(p)
+
+        if not population.extinct():
+            population.update_live_players()
+        else:
+            pass
 
         clock.tick(60)
         pygame.display.flip()
